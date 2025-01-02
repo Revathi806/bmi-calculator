7de56753f5f833da04b794852f441f598d6efec3
Revathi806 <revathirajesh242@gmail.com>
2025-01-02 08:27:12 +0530

    Delete bmi_project directory

--- bmi_project/bmi_calculator/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views  # Import your views module correctly
-
-urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('bmi/', views.calculate_bmi, name='calculate_bmi'),
-]
